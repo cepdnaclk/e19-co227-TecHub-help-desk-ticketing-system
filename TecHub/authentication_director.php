6abbadcf43a1081d052928bfca740389bf856b81
Pasindu Rangana <115542129+PasinduRR@users.noreply.github.com>
2023-10-17 23:46:12 +0530

    update email and authentication

--- TecHub/authentication_director.php
+++ TecHub/authentication_director.php
@@ -3,6 +3,10 @@ session_start();
 
 if (!isset($_SESSION['authenticated_director'])) {
     $_SESSION['status'] = "Please login to continue";
+    unset($_SESSION['authenticated_customer']);
+    unset($_SESSION['authenticated_to']);
+    unset($_SESSION['authenticated_admin']);
+    unset($_SESSION['authenticated_engineer']);
     header("Location: sign-in.php");
     exit(0);
 }
