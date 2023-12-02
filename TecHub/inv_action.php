dee01d568192aba958ae60b963017b817124fb46
NilakshiInduwara <115539831+NilakshiInduwara@users.noreply.github.com>
2023-12-02 03:36:08 -0800

    Update inv_action.php

--- TecHub/inv_action.php
+++ TecHub/inv_action.php
@@ -1,22 +1,31 @@
 <?php
-    try{
-        $conn = new PDO("mysql:host=localhost;dbname=techub","root","");
+try {
+    $conn = new PDO("mysql:host=localhost;dbname=techub", "root", "");
 
-        foreach($_POST['Inv_des'] as $key => $value){
-            $sql = "INSERT INTO invoice(InvoiceDes, Amount) VALUES (:Inv_des, :Amountt)";
-            $stmt = $conn->prepare($sql);
-            
-            $stmt -> execute([
-                "Inv_des"=> $value,
-                "Amountt"=> $_POST["Amountt"][$key],
-            ]);
-        }
-        echo "Invoice created successfully.";
-    } 
-    catch (Exception $e) {
-        echo "Error: " . $e->getMessage();
+    foreach ($_POST['Inv_des'] as $key => $value) {
+        $sql = "INSERT INTO invoice(InvoiceDes, Amount) VALUES (:Inv_des, :Amountt)";
+        $stmt = $conn->prepare($sql);
+
+        $stmt->execute([
+            "Inv_des" => $value,
+            "Amountt" => $_POST["Amountt"][$key],
+        ]);
+
+        // Retrieve the generated invoice ID
+        $invoiceId = $conn->lastInsertId();
+
+        // Update the ticket table with the invoice ID
+        $sql = "UPDATE ticket SET InvoiceId = :invoiceId WHERE TicketId = :ticketID";
+        $stmt = $conn->prepare($sql);
+
+        $stmt->execute([
+            "invoiceId" => $invoiceId,
+            "ticketID" => $_POST['ticketID'][$key],
+        ]);
     }
-
-    /* print_r($_POST); */
+    echo "Invoices created and ticket table updated successfully.";
+} 
+catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
 ?>
-
